8028d93a7d7fa0c91cc55d79a8f46d4e08b15c60
Pravendra Singh <hackpravj@gmail.com>
2015-01-09 19:35:40 +0530

    Add the collected process log

---
+++ R/ordered-languages.R
@@ -0,0 +1,4 @@
+# repository dataset
+stats.df <- read.csv("data/stats.csv")
+
+# language.ordered.df <- stats.df[with(stats.df, order(-languages)), ]
--- R/stars-and-languages.R
+++ R/stars-and-languages.R
@@ -1,8 +1,10 @@
 # load required libraries
 library("ggplot2")
 
+# repository dataset
 stats.df <- read.csv("data/stats.csv")
 
+# create a new point graph
 gplot <- ggplot(stats.df, aes(x = languages, y = stars))
 gplot <- gplot + labs(x = "No. of Programming Languages", y = "No. of GitHub Stars")
 gplot <- gplot + geom_point()
